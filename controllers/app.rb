cdb05aac2d50350465f37f938fceee21e8c0a181
aidahalitaj <aida.halitaj@uni-pr.edu>
2018-05-19 19:46:38 +0800

    setting route for assets

--- controllers/app.rb
+++ controllers/app.rb
@@ -17,7 +17,9 @@ module Wefix
     use Rack::Session::Cookie,
         expire_after: ONE_MONTH,
         secret: config.SESSION_SECRET
-
+    route do |routing|
+      routing.assets
+    end
     route do |routing|
       @current_account = session[:current_account]
 
